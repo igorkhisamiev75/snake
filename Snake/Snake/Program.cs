4749948159b8ff2a22becd45214fbf3e9419e642
igorkhisamiev75 <khisamievigor@gmail.com>
2021-01-15 13:36:02 +0300

    lesson10
    Змейка в движении

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Snake
 {
@@ -27,7 +28,18 @@ namespace Snake
             Snake snake = new Snake(p1, 4, Direction.RIGHT);
             snake.Draw();
 
-            
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+
+
 
             Console.ReadLine();
             
