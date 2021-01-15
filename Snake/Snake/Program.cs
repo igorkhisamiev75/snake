884acc9aab41ee8c301c63a31d147fdd7574fa21
igorkhisamiev75 <khisamievigor@gmail.com>
2021-01-15 14:42:02 +0300

    lesson11
    Добавляем считывание с клавиатуры

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -28,18 +28,17 @@ namespace Snake
             Snake snake = new Snake(p1, 4, Direction.RIGHT);
             snake.Draw();
 
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-
-
+            while(true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+                
+            }
 
             Console.ReadLine();
             
