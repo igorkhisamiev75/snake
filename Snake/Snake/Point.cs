d0cbd819a8927075e0b5ac721fcff98ac30acdfe
igorkhisamiev75 <khisamievigor@gmail.com>
2021-01-15 16:58:04 +0300

    lesson12
    Добавили еду

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -45,6 +45,12 @@ namespace Snake
                 y = y + offset;
             }
         }
+
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
